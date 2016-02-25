f622c27766563f74062d788337c7bdcf51d24fae
oonashvili <oonashvili@gmail.com>
2016-02-25 16:58:40 +0400

    Update Copy_Image_Between_Subs_ARM.ps1

--- Copy_Image_Between_Subs_ARM.ps1
+++ Copy_Image_Between_Subs_ARM.ps1
@@ -5,14 +5,14 @@ Select-AzureSubscription "MySubscription-PAYG"
 
 # Source Storage Account Information #
 $sourceStorageAccountName = "testvm23rg6812"
-$sourceKey = "9KvryAO/iYYQMLHR8arp54Y62bm8J/rJozHVOxd5T8hqt18Pr6B11tTKqaTcBNx11wJ3jeZV4GRkV4kr9B56eQ=="
-$sourceContext = New-AzureStorageContext –StorageAccountName $sourceStorageAccountName -StorageAccountKey $sourceKey  
-$sourceURI = "https://testvm23rg6812.blob.core.windows.net/system/Microsoft.Compute/Images/vhds/Template-osDisk.3c90a0e8-3ecd-4020-a086-dfdd4ef543c4.vhd"
+$sourceKey = "9KvryAO/iYYQM/zHV+Oxd5Tx11wJ3jeZV4GRkV4kr9B56eQ=="
+$sourceURI = "https://testvm23rg6812.blob.core.windows.net/system/Microsoft.Compute/Images/vhds/osDisk.vhd"
+$sourceContext = New-AzureStorageContext â€“StorageAccountName $sourceStorageAccountName -StorageAccountKey $sourceKey 
 
 # Destination Storage Account Information #
 $destinationStorageAccountName = "testvm67rg3874"
-$destinationKey = "OfjNs7y9KHiF8Hfr1RrBlgqNJXlzASD+KnsKFME+KDG+5TqpWtWWG8YimnXpJ4x1EIwQepnLeK8D1x+QAh+VCQ=="
-$destinationContext = New-AzureStorageContext –StorageAccountName $destinationStorageAccountName -StorageAccountKey $destinationKey
+$destinationKey = "9KvryAO/iY+5HurzHV+Oxd5Tx11wkkdjeZV4GRkm8kr9B56eQ=="
+$destinationContext = New-AzureStorageContext â€“StorageAccountName $destinationStorageAccountName -StorageAccountKey $destinationKey
 
 # Destination VHD blob #
 $blobName = "New-osDisk.3c90a0e8-3ecd-4020-a086-dfdd4ef543c4.vhd" 
